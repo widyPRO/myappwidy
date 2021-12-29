da03b77abee0361235fa5564b2971a09d2654708
Muhamad Widyantoro <50330166+widyPRO@users.noreply.github.com>
2021-12-29 22:49:02 +0700

    web static

---
+++ profile.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>W | Dunia - IT</title>
+<style type="text/css">
+<!--
+body {
+	background-color: #999999;
+}
+a:link {
+	color: #333333;
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+	color: #333333;
+}
+a:hover {
+	text-decoration: none;
+	color: #FFFFFF;
+}
+a:active {
+	text-decoration: none;
+	color: #333333;
+}
+-->
+</style></head>
+
+<body>
+<table width="780" height="200" border="0" align="center">
+  <tr>
+    <th bgcolor="#FFFFFF" scope="col"><img src="baner.jpg" width="775" height="200" /></th>
+  </tr>
+</table>
+<table width="780" border="0" align="center">
+  <tr>
+    <th bgcolor="#1fb7e9" scope="col"><div align="left"><a href="index.php">BERANDA</a> | <a href="profile.php">PROFIL</a> | <a href="galery.php">GALERI</a> | <a href="intro.php">BLOG</a></div></th>
+  </tr>
+</table>
+<table width="780" border="0" align="center">
+  <tr>
+    <th width="200" align="center" valign="top" bgcolor="#e647e0" scope="col"><table width="200" border="0" align="center">
+  <tr>
+    <th bgcolor="#1fb7e9" scope="col">TOPIK</th>
+    </tr>
+    </table>
+      <ul type="">
+          <li>
+            <div align="left"><a href="intro.php">Intro</a></div>
+          </li>
+          <li>
+            <div align="left"><a href="pengertian.php">Pengertian</a></div>
+          </li>
+          <li>
+            <div align="left"><a href="sejarah.php">Sejarah</a></div>
+          </li>
+          <li>
+            <div align="left"><a href="fungsi.php">Fungsi</a></div>
+          </li>
+          
+      </ul>
+     
+    </th>
+    <th width="570" align="center" valign="top" bgcolor="#FFFFFF" scope="col">
+      <h3>BIODATA SAYA</h3>
+      <p align="center"><img src="widy.jpg" width="300" height="350" />
+      
+      <table width="370" border="0" align="left">
+        <tr>
+          <td width="62" align="right" valign="top"><div align="left">Nama</div></td>
+          <td width="10">:</td>
+          <td width="284">Muhamad Widyantoro </td>
+        </tr>
+        <tr>
+          <td align="right" valign="top"><div align="left">NIM</div></td>
+          <td>:</td>
+          <td>205410038</td>
+        </tr>
+        <tr>
+          <td align="right" valign="top"><div align="left">TTL</div></td>
+          <td>:</td>
+          <td>Kebumen, 25 Maret 2001 </td>
+        </tr>
+        <tr>
+          <td align="right" valign="top"><div align="left">Alamat</div></td>
+          <td valign="top">:</td>
+          <td>Kebumen,Jawa Tengah,Indonesia. </td>
+        </tr>
+        <tr>
+          <td align="right" valign="top"><div align="left">Kampus</div></td>
+          <td>:</td>
+          <td>Universias Teknologi Digital Indonesia </td>
+        </tr>
+        <tr>
+          <td align="right" valign="top"><div align="left">No Ponsel </div></td>
+          <td>:</td>
+          <td>085741710084</td>
+        </tr>
+        <tr>
+          <td align="right" valign="top"><div align="left">Email</div></td>
+          <td>:</td>
+          <td>widy45rr@gmail.com</td>
+        </tr>
+        <tr>
+          <td height="21" align="right" valign="top"><div align="left">Agama</div></td>
+          <td>:  </td>
+          <td>Islam</td>
+        </tr>
+      </table>
+    </th>
+  </tr>
+</table>
+<table width="780" height="20" border="0" align="center">
+  <tr>
+    <th bgcolor="#77f877" scope="col"><h5>&copy; 2021 Muhamad Widyantoro</h5><script type='text/javascript' src='//eclkmpsa.com/adServe/banners?tid=94091_154020_0&tagid=2'></script> <br> <script type='text/javascript' src='//eclkmpbn.com/adServe/banners?tid=94091_154020_2'></script></th>
+  </tr>
+</table>
+</body>
+</html>
---
+++ sejarah.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>W | Dunia - IT</title>
+<style type="text/css">
+<!--
+body {
+	background-color: #999999;
+}
+a:link {
+	color: #333333;
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+	color: #333333;
+}
+a:hover {
+	text-decoration: none;
+	color: #FFFFFF;
+}
+a:active {
+	text-decoration: none;
+	color: #333333;
+}
+-->
+</style></head>
+
+<body>
+<table width="780" height="200" border="0" align="center">
+  <tr>
+    <th bgcolor="#FFFFFF" scope="col"><img src="baner.jpg" width="775" height="200" /></th>
+  </tr>
+</table>
+<table width="780" border="0" align="center">
+  <tr>
+    <th bgcolor="#1fb7e9" scope="col"><div align="left"><a href="index.php">BERANDA</a> | <a href="profile.php">PROFIL</a> | <a href="galery.php">GALERI</a> | <a href="intro.php">BLOG</a></div></th>
+  </tr>
+</table>
+<table width="780" border="0" align="center">
+  <tr>
+    <th width="200" align="center" valign="top" bgcolor="#e647e0" scope="col"><table width="200" border="0" align="center">
+  <tr>
+    <th bgcolor="#1fb7e9" scope="col">TOPIK</th>
+	  </tr>
+	  </table>
+      <ul type="">
+          <li>
+            <div align="left"><a href="intro.php">Intro</a></div>
+          </li>
+          <li>
+            <div align="left"><a href="pengertian.php">Pengertian</a></div>
+          </li>
+          <li>
+            <div align="left"><a href="sejarah.php">Sejarah</a></div>
+          </li>
+          <li>
+            <div align="left"><a href="fungsi.php">Fungsi</a></div>
+          </li>
+        
+      </ul>
+     
+    </th>
+    <th width="570" align="center" valign="top" bgcolor="FFFFFF" scope="col">
+      <h3>|SEJARAH|</h3>
+      <p align="justify">Sejarah website pertama kali dimulai dari seorang ilmuwan yang berasal dari Inggris, bernama Tim Berners-Lee. Orang tua dari Berners juga merupakan ilmuwan komputer pada era awal dunia komputasi.Tujuan awal dari Tim Berners membuat sebuah website adalah supaya lebih memudahkan para peneliti di tempat kerjanya untuk mendapatkan dan bertukar informasi. Kemudian, pada tanggal 30 April 1993, secara resmi CERN yang merupakan laboratorium fisika di Swiss mengumumkan tentang perilisan website secara gratis.
+        <h2 ><font color="red">
+         Untuk kembali ke beranda</h2><a href="index.php" font color="red">klik disini
+      </a>
+      
+    </th>
+  </tr>
+</table>
+<table width="780" height="20" border="0" align="center">
+  <tr>
+    <th bgcolor="#77f877" scope="col"><h5>&copy; 2021 Muhamad Widyantoro</h5><script type='text/javascript' src='//eclkmpsa.com/adServe/banners?tid=94091_154020_0&tagid=2'></script> <br> <script type='text/javascript' src='//eclkmpbn.com/adServe/banners?tid=94091_154020_2'></script></th>
+  </tr>
+</table>
+</body>
+</html>
